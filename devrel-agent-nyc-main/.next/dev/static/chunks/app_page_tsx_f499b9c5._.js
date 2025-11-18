8c38347a3bd1c2c7de134938bd01ed937cfe8c5c
Luke Cheng <2258420+chenglu@users.noreply.github.com>
2025-11-18 23:11:05 +0000

    add gitignore

--- devrel-agent-nyc-main/.next/dev/static/chunks/app_page_tsx_f499b9c5._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_02833984._.js",
-  "static/chunks/node_modules__pnpm_fa22b35a._.js"
-],
-    source: "dynamic"
-});
